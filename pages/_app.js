6d791dac0e49db1b17f2b6ead5be0abd22d2eee3
William <sart.william@gmail.com>
2024-11-24 10:39:34 +0100

    create store redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import user from '../reducers/user';
+
+const reducers = combineReducers({ user });
+
+const persistConfig = { key: 'loiseaublanc', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>L'Oiseau Blanc</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
