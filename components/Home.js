347615a37f4735404413f40ae82c909d578a0cfa
William <sart.william@gmail.com>
2024-11-24 20:21:33 +0100

    prépa css home

--- components/Home.js
+++ components/Home.js
@@ -10,12 +10,8 @@ function Home() {
   const router = useRouter();
 
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.home}>
+
     </div>
   );
 }
